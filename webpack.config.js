ed0b60356a4ca4b43aa915796c10e7cfa2887636
Ka Yi Leung <email@email.com87kleung@gmail.comgit config user.email>
2022-02-15 18:27:46 -0800

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+    context: __dirname,
+    entry: "./frontend/giftsy_redux.jsx",
+    output: {
+        path: path.join(__dirname),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: [/\.jsx?$/, /\.js?$/],
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                }
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: [".jsx", ".js", "*"]
+    }
+};
