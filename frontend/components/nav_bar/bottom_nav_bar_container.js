6ea7552968054e1c81d212ea19b0914b3fb14ada
Ka Yi Leung <email@email.com87kleung@gmail.comgit config user.email>
2022-03-03 23:48:42 -0800

    pushing to heroku

--- frontend/components/nav_bar/bottom_nav_bar_container.js
+++ frontend/components/nav_bar/bottom_nav_bar_container.js
@@ -1,19 +1,19 @@
-import { connect } from "react-redux";
-import { fetchCategory, fetchAllCategories } from "../../util/category_api_util";
-import BottomNavBar from "./bottom_nav_bar";
+// import { connect } from "react-redux";
+// import { fetchCategory, fetchAllCategories } from "../../util/category_api_util";
+// import BottomNavBar from "./bottom_nav_bar";
 
 
-const mSTP = state => {
-    return {
-        categories: Object.values(state.entities.categories)
-    }
-}
+// const mSTP = state => {
+//     return {
+//         categories: Object.values(state.entities.categories)
+//     }
+// }
 
-const mDTP = dispatch => {
-    return {
-        fetchAllCategories: () => dispatch(fetchAllCategories()),
-        fetchCategory: categoryId => dispatch(fetchCategory(categoryId))
-    }
-}
+// const mDTP = dispatch => {
+//     return {
+//         fetchAllCategories: () => dispatch(fetchAllCategories()),
+//         fetchCategory: categoryId => dispatch(fetchCategory(categoryId))
+//     }
+// }
 
-export default connect(mSTP, mDTP)(BottomNavBar)
+// export default connect(mSTP, mDTP)(BottomNavBar)
