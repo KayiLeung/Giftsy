dbfc7a18d0038dd5e9e4aec17a9de165ad23f098
Ka Yi Leung <email@email.com87kleung@gmail.comgit config user.email>
2022-03-04 11:06:58 -0800

    comment out orders controller

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,12 +1,12 @@
-class Api::OrdersController < ApplicationController
+# class Api::OrdersController < ApplicationController
     
-   def create
-    @order = Orders.new(order_params)
+#    def create
+#     @order = Orders.new(order_params)
 
-    if @Orders.save
-        render json: ['purchased']
-    else
-        render json: @order.errors.full_messages
-   end
+#     if @Orders.save
+#         render json: ['purchased']
+#     else
+#         render json: @order.errors.full_messages
+#    end
 
-end
+# end
