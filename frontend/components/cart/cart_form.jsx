922fde5f1c4eacfcab2ded3d0f01dc404e8726a1
Ka Yi Leung <email@email.com87kleung@gmail.comgit config user.email>
2022-04-06 17:19:51 -0700

    cart form

---
+++ frontend/components/cart/cart_form.jsx
@@ -0,0 +1,85 @@
+import { tsThisType } from "@babel/types";
+import React, { Componet } from "react";
+
+class Cartform extends React.Component {
+    constructor(props) {
+        super(props)
+        console.log(this.props.product.price)
+
+        this.state = {
+            quantity: 1,
+        }
+
+        this.addCartItem = this.addCartItem.bind(this)
+    }
+
+
+    addCartItem(e) {
+
+        e.preventDefault();
+        debugger
+        if (!this.props.currentUser) {
+            this.props.openModal('login')
+        } else {
+            debugger
+            const buyerId = this.props.currentUser;
+            const productId = this.props.product.id;
+            const amount = (this.props.product.price * this.state.quantity)
+
+            const cart = Object.assign({}, {
+                ...this.state,
+                buyer_id: buyerId,
+                product_id: productId,
+                amount: amount
+            })
+            this.props.createCart(buyerId, cart)
+        }
+
+    }
+
+
+
+
+
+    componentDidMount() {
+        // this.props.fetchAllCarts(this.props.currentUser)
+        // console.log(this.props.fetchAllCarts(this.props.currentUser))
+    }
+
+    update(field) {
+        debugger
+        return e => {
+            this.setState({
+                [field]: e.target.value
+            })
+        }
+    }
+
+    render() {
+        debugger
+        const { currentUser, cart } = this.props
+        console.log(`this is the first cart: ${cart}`)
+        if (!this.props.product) return null
+        debugger
+        return (
+            <div>
+                <form className='order-form-wrapper' onSubmit={this.addCartItem}>
+                    <select name="amount" id="item_quantity" onChange={this.update('quantity')}>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                    </select>
+                    <button className='cartItemBtn' type='submit'>Add to cart</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Cartform
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -4,13 +4,15 @@ import categoryReducer from "./categories_reducer";
 import productReducer from "./product_reducer";
 import usersReducer from "./users_reducer";
 import ratingReducer from "./ratings_reducer";
+import cartReducer from "./cart_reducer";
 
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     products: productReducer,
     categories: categoryReducer,
-    ratings: ratingReducer
+    ratings: ratingReducer,
+    carts: cartReducer
 })
 
 export default entitiesReducer;
