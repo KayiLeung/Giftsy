80fd0056837cd1c28a8955ea6529df39af02a2f9
Ka Yi Leung <email@email.com87kleung@gmail.comgit config user.email>
2022-04-06 17:59:41 -0700

    carts reducer

---
+++ frontend/reducers/carts_reducer.js
@@ -0,0 +1,25 @@
+import { RECEIVECART, DELETECART, RECEIVEALLCARTS } from '../actions/cart_action'
+
+const cartReducer = (state = {}, action) => {
+    Object.freeze(state);
+    const nextState = Object.assign({}, state);
+
+    switch (action.type) {
+        case RECEIVEALLCARTS:
+            return action.carts
+
+        case RECEIVECART:
+            debugger
+            nextState[action.cart.id] = action.rating;
+            return nextState;
+
+        case DELETECART:
+            delete nextState[action.cartId];
+            return nextState
+
+        default:
+            return state
+    }
+}
+
+export default cartReducer;
