426dcd29334bd534d14a2e096e6acfede1657231
Ka Yi Leung <email@email.com87kleung@gmail.comgit config user.email>
2022-04-25 12:36:54 -0700

    payment fix

--- frontend/components/order/order_form.jsx
+++ frontend/components/order/order_form.jsx
@@ -7,24 +7,46 @@ import { Link } from 'react-router-dom'
 class OrderForm extends React.Component {
     constructor(props){
         super(props)
+        this.state =  {
+            payment : null
+        }
  
         this.handleCheckOut = this.handleCheckOut.bind(this)
     }
 
     handleCheckOut(e){
         e.preventDefault();
-        
+        console.log(`handlecheckout :${this.state.payment}`)
         const {carts, deleteCart} = this.props
-        if (carts.length === 0) {
+        // if (carts.length === 0) {
+        //     alert("Cart is empty")
+        // } else {
+        //     carts.map((cart) =>{
+        //         deleteCart(cart.id)
+        //     }) 
+        //     .then(alert('Order Placed!'))
+        //     .then(<Redirect to='/'/>)
+        // }
+
+        if (carts.length === 0 ) {
             alert("Cart is empty")
+        } else if (this.state.payment === null) {
+            alert('Please select payment method')
         } else {
-            carts.map((cart) =>{
+            carts.map((cart) => {
                 deleteCart(cart.id)
-            }) 
-            .then(alert('Order Placed!'))
-            .then(<Redirect to='/'/>)
+            })
+                .then(alert('Order Placed!'))
+                .then(<Redirect to='/' />)
+
         }
             
+    }
+
+    handlePayment(type) {
+        return e => this.setState({
+            [type]: e.currentTarget.value
+        })
     }
 
 
@@ -41,11 +63,9 @@ class OrderForm extends React.Component {
                 <ul>
                     <li className='order-header'>How you'll pay</li>
                     <form className="place-order">
-                        <div className='radio-button' >
-                            <input type='radio' name='payment'/><label><img width='50%'src="https://giftsy-aa-seeds.s3.us-west-1.amazonaws.com/25928-1-credit-card-visa-and-master-card-file.png" alt="" /></label>
-                        </div>
-                        <div className='radio-button' >
-                            <input type="radio" name='payment' /><label><img width='25%'src="https://giftsy-aa-seeds.s3.us-west-1.amazonaws.com/580b57fcd9996e24bc43c530.png" alt="" /></label>
+                        <div className='radio-button' onChange={this.handlePayment('payment')}>
+                            <input type='radio' name='payment' value='visa'/><label><img width='50%'src="https://giftsy-aa-seeds.s3.us-west-1.amazonaws.com/25928-1-credit-card-visa-and-master-card-file.png" alt="" /></label>
+                            <input type="radio" name='payment' value='paypal'/><label><img width='50%'src="https://giftsy-aa-seeds.s3.us-west-1.amazonaws.com/580b57fcd9996e24bc43c530.png" alt="" /></label>
                         </div>
               
                     <div className='total-amount'>
